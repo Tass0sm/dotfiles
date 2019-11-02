242a0d4fcb48a496436768605f8a710d049d8ec9
Anastasios <tassos.manganaris@gmail.com>
2019-11-02 18:34:15 -0400

    Some environment and Emacs changes.

---
+++ emacs.d/elpa/csharp-mode-0.9.2/csharp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csharp-mode" "0.9.2" "C# mode derived mode" 'nil :commit "5e47b7764b3f4c97c260a902e8072d444dbd0f1b" :keywords '("c#" "languages" "oop" "mode") :authors '(("Dylan R. E. Moonfire (original)")) :maintainer '("Jostein Kjønigsen" . "jostein@gmail.com") :url "https://github.com/josteink/csharp-mode")
