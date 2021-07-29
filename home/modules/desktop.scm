20157b171cc5d01bfd0be1ec6ac8945ae0cc87d7
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-07-28 20:04:53 -0400

    Completed first bspwm and sxhkd services

--- home/modules/desktop.scm
+++ home/modules/desktop.scm
@@ -1,18 +1,22 @@
 (define-module (home modules desktop)
-  #:use-module (guix gexp)
   #:use-module (gnu home)
   #:use-module (gnu packages)
   #:use-module (gnu services)
   #:use-module (gnu home-services)
   #:use-module (gnu home-services-utils)
-  #:use-module (gnu home-services files)
 
+  #:use-module (guix gexp)
   #:use-module (tassos-guix home-services desktop))
 
 (define-public desktop-services
   (list
    (service home-sxhkd-service-type
-	    (home-sxhkd-configuration))
+	    (home-sxhkd-configuration
+	     (sxhkdrc (list
+		       (slurp-file-gexp
+			(local-file "../files/sxhkdrc"))))))
    (service home-bspwm-service-type
 	    (home-bspwm-configuration
-	     (bspwmrc (list (slurp-file-gexp (local-file "../files/bspwmrc"))))))))
+	     (bspwmrc (list
+		       (slurp-file-gexp
+			(local-file "../files/bspwmrc"))))))))
