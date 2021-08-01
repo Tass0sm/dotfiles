d941897d1f00b756c5f9df78e6d2f4ba931ff4e0
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-08-01 18:56:40 -0400

    Added dunst and xresources services

--- home/modules/desktop.scm
+++ home/modules/desktop.scm
@@ -4,12 +4,28 @@
   #:use-module (gnu services)
   #:use-module (gnu home-services)
   #:use-module (gnu home-services-utils)
+  #:use-module (gnu home-services xorg)
 
   #:use-module (guix gexp)
-  #:use-module (tassos-guix home-services desktop))
+  #:use-module (tassos-guix home-services wm)
+  #:use-module (tassos-guix home-services notifications))
 
 (define-public desktop-services
   (list
+   (service home-xresources-service-type
+	    (home-xresources-configuration
+	     (config
+	      `((include . "\"/home/tassos/.config/guix/home/files/nord-xresources\"")))))
+   (service home-dunst-service-type
+	    (home-dunst-configuration
+	     (dunstrc (list
+		       (slurp-file-gexp
+			(local-file "../files/dunstrc"))))))
+   ;; (service home-polybar-service-type
+   ;; 	    (home-polybar-configuration
+   ;; 	     (config (list
+   ;; 		      (slurp-file-gexp
+   ;; 		       (local-file "../files/polybar-config"))))))
    (service home-sxhkd-service-type
 	    (home-sxhkd-configuration
 	     (sxhkdrc (list
