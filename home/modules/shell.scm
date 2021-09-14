a1352aace282394fa50b4cbbd0c673b0c9e9078f
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-09-14 00:50:15 -0400

    Fixed config for not using login shell and a normal Guix setup.

--- home/modules/shell.scm
+++ home/modules/shell.scm
@@ -16,11 +16,23 @@
 	     (xdg-flavor? #t)
 	     (environment-variables
 	      '(("EDITOR" . "\"emacsclient -a ''\"")
+		("XCURSOR_THEME" . "Nordzy-cursors")
 		("GUIX_LOCPATH" . "$HOME/.guix-profile/lib/locale")
 		("SSL_CERT_DIR" . "$HOME/.guix-home/profile/etc/ssl/certs")
 		("SSL_CERT_FILE" . "$HOME/.guix-home/profile/etc/ssl/certs/ca-certificates.crt")
-		("GIT_SSL_CAINFO" . "$SSL_CERT_FILE")))
+		("GIT_SSL_CAINFO" . "$SSL_CERT_FILE")
+		("GEM_PATH" . "$HOME/.local/share/gem")))
 	     (zshrc
-	      (list (slurp-file-gexp (local-file "../files/zshrc"))))
-	     (zprofile
-	      (list (slurp-file-gexp (local-file "../files/zprofile"))))))))
+	      (list (slurp-file-gexp (local-file "../files/zshrc"))))))
+   (simple-service 'login-variables
+		   home-environment-variables-service-type
+		   `(;; ("XDG_DATA_DIRS" . "$XDG_DATA_DIRS:/usr/local/share/:/usr/share/")
+		     ;; ("XDG_CONFIG_DIRS" . "$XDG_CONFIG_DIRS:/etc/xdg/")
+		     ;; ("XDG_DATA_DIRS" . "$HOME/.guix-home/profile/share:$XDG_DATA_DIRS")
+		     ;; ("XDG_CONFIG_DIRS" . "$HOME/.guix-home/profile/etc/xdg:$XDG_CONFIG_DIRS")
+		     ("GUILE_LOAD_PATH" . "$XDG_CONFIG_HOME/guix/current/share/guile/site/3.0:$GUILE_LOAD_PATH")
+		     ("GUILE_LOAD_COMPILED_PATH" . "$XDG_CONFIG_HOME/guix/current/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH")
+		     ("PATH" . "$HOME/.local/bin/:$PATH")
+		     ;; ("GUIX_PROFILE=\"$HOME/.guix-profile\"" . #f)
+		     ;; [ -f "$GUIX_PROFILE/etc/profile" ] && . "$GUIX_PROFILE/etc/profile"
+		     ))))
